9de2c73b7e163217b3e113a6270eeaf1dd47bb17
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-24 20:32:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchBar from "./Components/SearchBar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="ui container">
+      <SearchBar />
+    </div>
+  );
+}
