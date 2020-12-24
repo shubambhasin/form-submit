1b804b446289b9aad6af81d4d704325f958b3653
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-24 23:45:31 +0530

    added api request method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import axios from "axios";
 import React from "react";
 import SearchBar from "./Components/SearchBar";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="ui container">
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID bQcIls2cUBgbuj9uuHx6z8hKR7GoTvUJSOTHsv44sPI"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+      //onsubmit above is not a defined term can be names as anything
+    );
+  }
 }
+
+export default App;
